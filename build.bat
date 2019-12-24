87a6936d1ddb3cfbf6c8aff9325c58f0bd805af7
silvioprog <silvioprog@gmail.com>
2019-12-24 12:04:31 -0300

    Added bat to make it easy to build on Windows using MSVC.

---
+++ build.bat
@@ -0,0 +1,5 @@
+:::::::::::::::::::::::::::::::::::::::::::
+:: Copyright (C) 2019 Duall Sistemas Ltda.
+:::::::::::::::::::::::::::::::::::::::::::
+
+cargo build --target=i686-pc-windows-msvc --release
