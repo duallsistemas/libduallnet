df7d4bb4ea47da0f6cf4a92b3708e256ce973455
silvioprog <silvioprog@gmail.com>
2020-01-08 17:09:36 -0300

    Linked CRT statically on Windows.

--- build.bat
+++ build.bat
@@ -2,4 +2,5 @@
 :: Copyright (C) 2019 Duall Sistemas Ltda.
 :::::::::::::::::::::::::::::::::::::::::::
 
+set RUSTFLAGS=-Ctarget-feature=+crt-static
 cargo build --target=i686-pc-windows-msvc --release
