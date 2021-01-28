e7dd019c8fa2d98d1ecb41e28bd1b68c5ad880ae
silvioprog <silvioprog@gmail.com>
2021-01-28 11:10:28 -0300

    Cleanup before compiling new binaries.

--- build.bat
+++ build.bat
@@ -1,7 +1,8 @@
-:::::::::::::::::::::::::::::::::::::::::::
-:: Copyright (C) 2019 Duall Sistemas Ltda.
-:::::::::::::::::::::::::::::::::::::::::::
+:::::::::::::::::::::::::::::::::::::::::::::::
+:: Copyright (C) 2019-2021 Duall Sistemas Ltda.
+:::::::::::::::::::::::::::::::::::::::::::::::
 
 set RUSTFLAGS=-Ctarget-feature=+crt-static
 rustup target add i686-pc-windows-msvc
+cargo clean
 cargo build --target=i686-pc-windows-msvc --release
